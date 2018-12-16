ada14dbbc1bcce33b1c5a05d96cb0e511fb7f018
Weng Kin Lee <wengkin2967@gmail.com>
2018-12-16 14:33:20 +0800

    Snake head can be seen on screen, edited some other parts of code

--- snake_game.py
+++ snake_game.py
@@ -6,15 +6,20 @@ window = turtle.Screen()
 window.title("Snake Game by Weng Kin Lee")
 window.bgcolor("green")
 window.setup(width=600, height=600)
-window.tracer(0)
+window.tracer(0) # Turns off screen updates
 
-window.mainloop()
 
 # Snake Head
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
-head.color('red')
+head.color("red")
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
+
+# Main Game Loop
+while True:
+    window.update()
+
+window.mainloop()
